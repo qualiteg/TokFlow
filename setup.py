48a10b65e741f0b986f591844438cd4b715a4fea
riversun.org@gmail.com <riversun.org@gmail.com>
2023-05-19 14:21:24 +0900

    update readme and version for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="TokFlow",
-    version="1.0.0",
+    version="1.0.1",
     author="Tom Misawa",
     author_email="riversun.org@gmail.com",
     description="LLM utility of streaming token realtime replacement processing",
